ab25694fe6520694724fa6bbdd943a72fdee1d27
David Castaño <dcastano@proveedor.intercorp.com.pe>
2023-06-19 21:00:02 -0500

    Initial Commit

---
+++ recipeList/recipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeListApp.swift
+//  recipeList
+//
+//  Created by david on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct recipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
